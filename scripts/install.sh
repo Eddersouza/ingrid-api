65c9386ea01ee621fc569a58de2bc464ebec3a80
ederson <eddye_souza@yahoo.com.br>
2026-03-05 14:20:51 -0300

    ajuste install

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,10 +5,6 @@ echo "Installing backend..."
 
 mkdir -p /var/www/backend
 
-chown -R ec2-user:ec2-user /var/www/backend
-
-chmod 644 /etc/systemd/system/backend.service
-
 systemctl daemon-reload
 
 echo "Install complete"
