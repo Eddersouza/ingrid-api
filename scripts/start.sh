bb4dd320ee00f636d1a436f0fd95da313faa7cbf
ederson <eddye_souza@yahoo.com.br>
2026-03-06 09:03:43 -0300

    alteração do start

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -euxo pipefail
 
+echo "Stopping old process..."
+
+sudo systemctl stop backend || true
+
+echo "Starting backend..."
+
+sudo systemctl start backend
+
 echo "Starting backend service..."
 
 systemctl enable backend
